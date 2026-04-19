6d0b1bd6a3020f538685ebeba0ea96a88c10b676
Fluent Immersion <158618477+teachermarcelo@users.noreply.github.com>
2026-04-19 14:50:51 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './',
+  base: '/finance-english-system/',
   plugins: [react(), tailwindcss()],
 })
