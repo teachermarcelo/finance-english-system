ba0e36d3f0269347f2061191deb2aab667ba4b07
Fluent Immersion <158618477+teachermarcelo@users.noreply.github.com>
2026-04-20 10:47:04 -0300

    Add files via upload

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,10 +1,1 @@
-import Sidebar from '../components/Sidebar'
-
-export default function AppLayout({ children }) {
-  return (
-    <div className="mx-auto grid min-h-screen max-w-7xl gap-6 p-4 md:grid-cols-[260px_1fr] md:p-6">
-      <Sidebar />
-      <main>{children}</main>
-    </div>
-  )
-}
+content for AppLayout
